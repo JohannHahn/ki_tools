5e66220de2c79fac05402ff4148927f03c73d39a
JohannH <johann-hahn@yandex.ru>
2015-08-04 21:13:56 +0200

    unnötige imports raus

--- core/src/com/mygdx/game/MainScreen.java
+++ core/src/com/mygdx/game/MainScreen.java
@@ -1,16 +1,11 @@
 package com.mygdx.game;
 
-import java.awt.TextComponent;
-
-import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.mygdx.components.PositionComponent;
