9adacf5de48a0b614aa0954428d53ea313efcc3c
Nik <Nik@10.143.105.12>
2015-08-04 12:03:17 +0200

    test

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,6 +8,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
-		//LAPTOP COMMENT
+		//LAPTOP COMMENT 2.0
 	}
 }
