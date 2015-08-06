a08b78eae9a7d0a8d7f3a5ea5633f97b13a2a4d2
Nik <Nik@Nik-HP.fritz.box>
2015-08-06 11:35:53 +0200

    is holding the information how the entity should be drawn

--- core/src/com/mygdx/components/RenderComponent.java
+++ core/src/com/mygdx/components/RenderComponent.java
@@ -1,9 +1,55 @@
 package com.mygdx.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Texture;
 
 public class RenderComponent extends Component {
-//obly objects with render components will be rendert
-	//Should hold the information how it shoud be drawen
-	
+	// only objects width render components will be rendert
+	// hold the information how it shoud be drawen
+
+	private Texture texture = null;// if null then draw default Shape in Rendersystem
+	private int width = 0;
+	private int height = 0;
+
+	public RenderComponent() {
+
+	}
+
+	public RenderComponent(Texture texture) {
+		this();
+		setHeight(texture.getHeight());
+		setWidth(texture.getWidth());
+		this.setTexture(texture);
+	}
+
+	public RenderComponent(Texture texture, int width, int height) {
+		this(texture);
+		setHeight(height);
+		setWidth(width);
+	}
+
+	public Texture getTexture() {
+		return texture;
+	}
+
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 }
