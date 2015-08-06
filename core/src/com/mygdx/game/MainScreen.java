d3e4d4f7a19a4dac65563c67b0216036fa26e8f2
Nik <Nik@Nik-HP.fritz.box>
2015-08-06 11:39:20 +0200

    changed the main texture source only seek behaviour

--- core/src/com/mygdx/game/MainScreen.java
+++ core/src/com/mygdx/game/MainScreen.java
@@ -13,7 +13,6 @@ import com.mygdx.components.FleeComponent;
 import com.mygdx.components.PositionComponent;
 import com.mygdx.components.RenderComponent;
 import com.mygdx.components.SeekComponent;
-import com.mygdx.components.TextureComponent;
 import com.mygdx.components.VelocityComponent;
 import com.mygdx.system.MovementSystem;
 import com.mygdx.system.RenderSystem;
@@ -21,7 +20,7 @@ import com.mygdx.system.RenderSystem;
 public class MainScreen implements Screen {
     Engine engine;
     Entity bild,bild2,bild3,bild4,bild5,bild6,bild7;
-    Texture text = new Texture("badlogic.jpg");
+    Texture text = new Texture("smiley.png");
     Stage stage = new Stage();
     Image img = new Image(text);
     MyGdxGame game;
@@ -32,56 +31,57 @@ public class MainScreen implements Screen {
         bild = new Entity();
 
         bild.add(new PositionComponent(MathUtils.random(0,600 ),MathUtils.random(0,600 )));
-        bild.add(new TextureComponent(text));
+       
         bild.add(new VelocityComponent());
         bild.add(new SeekComponent());  
-        bild.add(new RenderComponent());
+        bild.add(new RenderComponent(text,40,40));
         
         bild2= new Entity();
         bild2.add(new PositionComponent(MathUtils.random(0,600 ),MathUtils.random(0,600 )));
-        bild2.add(new TextureComponent(text));
+        
         bild2.add(new SeekComponent());
         bild2.add(new VelocityComponent());
         bild2.add(new RenderComponent());
  
         bild3 = new Entity();
         bild3.add(new PositionComponent(MathUtils.random(0,600 ),MathUtils.random(0,600 )));
-        bild3.add(new TextureComponent(text));
+        
         bild3.add(new VelocityComponent());
-        bild3.add(new FleeComponent());
+        bild3.add(new SeekComponent());
         bild3.add(new RenderComponent());
 
         bild4= new Entity();
         bild4.add(new PositionComponent(MathUtils.random(0,600 ),MathUtils.random(0,600 )));
-        bild4.add(new TextureComponent(text));
+        
         bild4.add(new VelocityComponent());
-        bild4.add(new FleeComponent());
+        bild4.add(new SeekComponent());
         bild4.add(new RenderComponent());
         
         bild5= new Entity();
         bild5.add(new PositionComponent(MathUtils.random(0,600 ),MathUtils.random(0,600 )));
-        bild5.add(new TextureComponent(text));
+        
         bild5.add(new VelocityComponent());
-        bild5.add(new FleeComponent());
+        bild5.add(new SeekComponent());
         bild5.add(new RenderComponent());
         
         bild6 = new Entity();
         bild6.add(new PositionComponent(MathUtils.random(0,600 ),MathUtils.random(0,600 )));
-        bild6.add(new TextureComponent(text));
+        
         bild6.add(new VelocityComponent());
-        bild6.add(new FleeComponent());
+        bild6.add(new SeekComponent());
         bild6.add(new RenderComponent());
         
         bild7= new Entity();
         bild7.add(new PositionComponent(MathUtils.random(0,600 ),MathUtils.random(0,600 )));
-        bild7.add(new TextureComponent(text));
+        
         bild7.add(new VelocityComponent());
-        bild7.add(new FleeComponent());
+        bild7.add(new SeekComponent());
         bild7.add(new RenderComponent());
         
         this.game = game;   
         engine.addSystem(new MovementSystem());
         engine.addSystem(new RenderSystem(game.batch));
+        
         engine.addEntity(bild);
         engine.addEntity(bild2);
         engine.addEntity(bild3);
