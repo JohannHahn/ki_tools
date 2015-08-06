75381a6ec8fa37d14200e943be7b83e4ec49205b
Nik <Nik@Nik-HP.fritz.box>
2015-08-06 11:36:43 +0200

    unnessecery

--- core/src/com/mygdx/components/TextureComponent.java
+++ core/src/com/mygdx/components/TextureComponent.java
@@ -4,7 +4,7 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.graphics.Texture;
 
 public class TextureComponent extends Component {
-	
+	//unnoetig
         public Texture txt;
         public TextureComponent(Texture txt){
             this.txt = txt;
