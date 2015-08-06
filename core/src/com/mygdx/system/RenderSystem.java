7d22ffcf09b0749de59700fd42834f15a4632fdd
Nik <Nik@Nik-HP.fritz.box>
2015-08-06 11:41:29 +0200

    if else statement dicides how the entity is be drawn

--- core/src/com/mygdx/system/RenderSystem.java
+++ core/src/com/mygdx/system/RenderSystem.java
@@ -6,8 +6,10 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.components.PositionComponent;
 import com.mygdx.components.RenderComponent;
@@ -15,38 +17,53 @@ import com.mygdx.components.TextureComponent;
 
 public class RenderSystem extends EntitySystem {
 
-    private ImmutableArray<Entity> entities;
-    private SpriteBatch batch;
-    private ShapeRenderer shapeRenderer;    
+	private ImmutableArray<Entity> entities;
+	private SpriteBatch batch;
+	private ShapeRenderer shapeRenderer;
 
-    //private ComponentMapper<com.mygdx.components.TextureComponent> tm = ComponentMapper.getFor(TextureComponent.class);
-    private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);    
+	// May be better deleted and use entity.getComponent
+	private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+	private ComponentMapper<RenderComponent> rm = ComponentMapper.getFor(RenderComponent.class);
 
-    public RenderSystem(SpriteBatch batch) {
-        this.batch = batch;
-        shapeRenderer= new ShapeRenderer();
-    }
+	public RenderSystem(SpriteBatch batch) {
+		this.batch = batch;
+		shapeRenderer = new ShapeRenderer();
+	}
 
-    public void addedToEngine(Engine engine) {
-        entities = engine.getEntitiesFor(Family.getFor(PositionComponent.class,RenderComponent.class));
-        System.out.println("Rendersystem added");
-                
-    }
-//Changed
-    public void update(float deltaTime) {
-        
-        for (int i = 0; i < entities.size(); ++i) {
-            Entity entity = entities.get(i);
-            PositionComponent positionComp = pm.get(entity);
-            Vector2 position = positionComp.position;
-            batch.begin();
-            shapeRenderer.setAutoShapeType(true);
-            shapeRenderer.begin();
-            
-            shapeRenderer.triangle(position.x, position.y, position.x+10, position.y,position.x+5, position.y+10);
-          //  batch.draw(texture.txt, position.x, position.y);
-            shapeRenderer.end();
-            batch.end();
-        }
-    }
+	public void addedToEngine(Engine engine) {
+		entities = engine.getEntitiesFor(Family.getFor(PositionComponent.class, RenderComponent.class));
+		System.out.println("Rendersystem added");
+
+	}
+
+	// Changed
+	public void update(float deltaTime) {
+		PositionComponent positionComp;
+		RenderComponent renderComp;
+		for (int i = 0; i < entities.size(); ++i) {
+			Entity entity = entities.get(i);
+			positionComp = pm.get(entity);
+			renderComp = rm.get(entity);
+
+			Vector2 position = positionComp.position;
+
+			batch.begin();
+
+			
+			if (renderComp.getTexture() != null) {
+				Texture tempTexture = renderComp.getTexture();
+				batch.draw(tempTexture, position.x, position.y, renderComp.getWidth(), renderComp.getHeight());
+			} else {
+				shapeRenderer.setAutoShapeType(true);
+				shapeRenderer.begin();
+
+				shapeRenderer.triangle(position.x, position.y, position.x + 10, position.y, position.x + 5,
+						position.y + 10);
+
+				shapeRenderer.end();
+			}
+
+			batch.end();
+		}
+	}
 }
