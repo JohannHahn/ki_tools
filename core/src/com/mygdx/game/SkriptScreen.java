40fb6f87cb419a1e57c4d3b45321bc25dd146fa6
Nik <Nik@Nik-HP>
2015-08-25 10:56:53 +0200

    BackgroundColor

--- core/src/com/mygdx/game/SkriptScreen.java
+++ core/src/com/mygdx/game/SkriptScreen.java
@@ -59,6 +59,7 @@ public class SkriptScreen implements Screen {
 	
 
 	public void render(float delta) {
+		Gdx.gl.glClearColor(1,1,0.5f, 1);//clear the screen	
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.act(delta);
 		stage.draw();
